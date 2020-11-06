ebfac89197ead7c81b6eeb272f9ddab22af713a7
samtamu6 <48324855+samtamu6@users.noreply.github.com>
2020-11-05 16:38:31 -0800

    Update hkpc-cwsj-mobile-converter-qa.tf

--- accounts/connonprodwsjapac/tokyo/hkpc-cwsj-mobile-converter-qa.tf
+++ accounts/connonprodwsjapac/tokyo/hkpc-cwsj-mobile-converter-qa.tf
@@ -47,35 +47,35 @@ resource "aws_security_group" "hkpc-cwsj-mobile-converter-qa" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = -1
     to_port     = -1
     protocol    = "icmp"
-    cidr_blocks = ["10.0.0.0/8"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 
