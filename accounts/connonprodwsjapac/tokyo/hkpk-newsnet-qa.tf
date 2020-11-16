8781e6f375546834e9cdaa34d3f94d013a1b83ac
samtamu6 <48324855+samtamu6@users.noreply.github.com>
2020-11-16 06:37:18 -0800

    Update hkpk-newsnet-qa.tf

--- accounts/connonprodwsjapac/tokyo/hkpk-newsnet-qa.tf
+++ accounts/connonprodwsjapac/tokyo/hkpk-newsnet-qa.tf
@@ -12,21 +12,21 @@ resource "aws_security_group" "hkpk-newsnet-qa" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]
+    cidr_blocks = ["10.0.0.0/8","172.26.0.0/16","192.168.0.0/16"]
   }
 
   ingress {
     from_port   = 80 
     to_port     = 80 
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]
+    cidr_blocks = ["10.0.0.0/8","172.26.0.0/16","192.168.0.0/16"]
   }
 
   ingress {
     from_port   = 443 
     to_port     = 443 
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]
+    cidr_blocks = ["10.0.0.0/8","172.26.0.0/16","192.168.0.0/16"]
   }
 
   ingress {
@@ -44,7 +44,7 @@ resource "aws_security_group" "hkpk-newsnet-qa" {
   }
 
   egress {
-    from_port   = 22
+    from_port   = 21
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
