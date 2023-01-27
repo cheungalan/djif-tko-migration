415e102491510403b9f010f5587f4c329d68aa45
samtamu6 <48324855+samtamu6@users.noreply.github.com>
2023-01-27 07:27:21 -0800

    Update hkpk-newsnet-qa.tf

--- accounts/connonprodwsjapac/tokyo/hkpk-newsnet-qa.tf
+++ accounts/connonprodwsjapac/tokyo/hkpk-newsnet-qa.tf
@@ -148,12 +148,12 @@ resource "aws_instance" "hkpk-newsnet-qa" {
 
     tags = {
         Name        = "${var.hkpk-newsnet-qa-name}${count.index + 1}" 
-        bu          = "${var.TagBU}"
+        bu          = "djcs"
         owner       = "${var.TagOwner}"
         environment = "${var.TagEnv}"
         product     = "${var.TagProduct}"
         component   = "${var.TagComponent}"
-        servicename = "${var.TagServiceName}"
+        servicename = "djcs/wsj/web"
         appid       = "${var.appid}"       
         preserve    = true
     }
