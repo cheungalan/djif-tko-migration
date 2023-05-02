68f2423a4babce18facc5a16080f23d563453111
cheungalan <alan.cheung@dowjones.com>
2023-05-02 15:38:23 +0800

    Update hkpc-cls-recv-qa.tf
    add ' autosnap = "bkp=a" ' to Tags

--- accounts/connonprodwsjapac/tokyo/hkpc-cls-recv-qa.tf
+++ accounts/connonprodwsjapac/tokyo/hkpc-cls-recv-qa.tf
@@ -204,7 +204,8 @@ resource "aws_instance" "hkpc-cls-recv" {
         product     = "${var.TagProduct}"
         component   = "${var.TagComponent}"
         servicename = "djin/newswires/web"
-        appid       = "in_newswires_djnews_clsdist"       
+        appid       = "in_newswires_djnews_clsdist"   
+        autosnap    = "bkp=a"
         preserve    = true
     }
 }
