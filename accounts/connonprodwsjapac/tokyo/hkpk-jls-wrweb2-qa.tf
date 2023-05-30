b2d8fe9a6364dc97df6e9c0eefc9cd5f6c98fd80
Ed Li <ed.li@dowjones.com>
2023-05-30 12:55:26 -0400

    CT-14959 Added define root volume to jls-wrweb2-qa

--- accounts/connonprodwsjapac/tokyo/hkpk-jls-wrweb2-qa.tf
+++ accounts/connonprodwsjapac/tokyo/hkpk-jls-wrweb2-qa.tf
@@ -1,27 +1,32 @@
 data "aws_security_group" "hkpk-jls-wrweb1-qa" {
-    filter {
-        name = "group-name"
-        values = ["hkpk-jls-wrweb1-qa"] 
-    }
+  filter {
+    name   = "group-name"
+    values = ["hkpk-jls-wrweb1-qa"]
+  }
 }
 
 resource "aws_instance" "hkpk-jls-wrweb2-qa" {
-    ami                    = "ami-0e35006f2980df05d"
-    instance_type          = "${var.instance_type}"
-    key_name               = "hkpk-jls-wrweb1-qa-key" 
-    subnet_id              = "subnet-076c0f9457edadfc9" 
-    vpc_security_group_ids = ["${data.aws_security_group.hkpk-jls-wrweb1-qa.id}"]
+  ami                    = "ami-0e35006f2980df05d"
+  instance_type          = var.instance_type
+  key_name               = "hkpk-jls-wrweb1-qa-key"
+  subnet_id              = "subnet-076c0f9457edadfc9"
+  vpc_security_group_ids = ["${data.aws_security_group.hkpk-jls-wrweb1-qa.id}"]
 
-    tags = {
-        Name        = "hkpk-jls-wrweb2-qa" 
-        bu          = "djin"
-        owner       = "${var.TagOwner}"
-        environment = "${var.TagEnv}"
-        product     = "${var.TagProduct}"
-        component   = "${var.TagComponent}"
-        servicename = "djin/newswires/web"
-        appid       = "in_newswires_web_jlswireryter"      
-        autosnap    = "bkp=a"
-        preserve    = true
-    }
+  root_block_device {
+    volume_size = var.root_v_size
+    volume_type = var.root_v_type
+  }
+
+  tags = {
+    Name        = "hkpk-jls-wrweb2-qa"
+    bu          = "djin"
+    owner       = "${var.TagOwner}"
+    environment = "${var.TagEnv}"
+    product     = "${var.TagProduct}"
+    component   = "${var.TagComponent}"
+    servicename = "djin/newswires/web"
+    appid       = "in_newswires_web_jlswireryter"
+    autosnap    = "bkp=a"
+    preserve    = true
+  }
 }
