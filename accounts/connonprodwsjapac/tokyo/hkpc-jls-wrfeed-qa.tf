6b50ee57ca6f51a9bdf2aaaba4f63b8238fc2faf
Ed Li <ed.li@dowjones.com>
2023-05-31 13:00:47 -0400

    NCTCOMPUTE-3013 - add SG rule description to hkpc-jls-wrfeed

--- accounts/connonprodwsjapac/tokyo/hkpc-jls-wrfeed-qa.tf
+++ accounts/connonprodwsjapac/tokyo/hkpc-jls-wrfeed-qa.tf
@@ -4,77 +4,84 @@ resource "aws_key_pair" "hkpc-jls-wrfeed-key" {
 }
 
 data "aws_security_group" "djif-default-hkpc-jls-wrfeed" {
-    filter {
-        name = "group-name"
-        values = ["djif_default"]
-    }
+  filter {
+    name   = "group-name"
+    values = ["djif_default"]
+  }
 }
 
 resource "aws_security_group" "hkpc-jls-wrfeed" {
   name        = "hkpc-jls-wrfeed"
   description = "hkpc-jls-wrfeed"
-  vpc_id      =  var.vpc_id 
+  vpc_id      = var.vpc_id
 
   ingress {
+    description = "Custom inbound from Newswires feeds"
     from_port   = 20000
     to_port     = 20100
     protocol    = "tcp"
-    cidr_blocks = ["10.150.86.0/23","10.150.88.0/23","10.243.6.0/24", "10.243.135.0/24", "10.151.54.0/23", "10.151.56.0/23"]
+    cidr_blocks = ["10.150.86.0/23", "10.150.88.0/23", "10.243.6.0/24", "10.243.135.0/24", "10.151.54.0/23", "10.151.56.0/23"]
   }
 
   ingress {
-    from_port   = 3389 
-    to_port     = 3389 
+    description = "RDP inbound Access from all internal"
+    from_port   = 3389
+    to_port     = 3389
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8","172.26.0.0/16","192.168.0.0/16"]
+    cidr_blocks = ["10.0.0.0/8", "172.26.0.0/16", "192.168.0.0/16"]
   }
 
   ingress {
-    from_port   = 21 
-    to_port     = 21 
+    description = "FTP inbound access from internal"
+    from_port   = 21
+    to_port     = 21
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/8"]
   }
 
   // FTP-Pasv
   ingress {
-    description = "FTP-Pasv"
+    description = "FTP-Pasv self inboudn access"
     from_port   = 5000
     to_port     = 5100
     protocol    = "tcp"
-    security_groups = ["sg-0f27d915082a302b7"]
-  } 
+    self        = true
+  }
 
-  
   ingress {
-    from_port   = 139 
-    to_port     = 139 
+    description = "NetBios inbound from internal"
+    from_port   = 139
+    to_port     = 139
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/8"]
   }
 
   ingress {
-    from_port   = 445 
-    to_port     = 445 
+    description = "SMB inbound from internal"
+    from_port   = 445
+    to_port     = 445
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/8"]
   }
 
   ingress {
-    from_port   = 1433 
-    to_port     = 1433 
+    description = "MSSQL inbound from internal"
+    from_port   = 1433
+    to_port     = 1433
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/8"]
   }
 
   ingress {
-    from_port   = -1 
-    to_port     = -1 
+    description = "ICMP from internal"
+    from_port   = -1
+    to_port     = -1
     protocol    = "icmp"
     cidr_blocks = ["10.0.0.0/8"]
   }
 
   egress {
+    description = "Dyanmic high ports outbound to DJ FTP"
     from_port   = 1024
     to_port     = 65535
     protocol    = "tcp"
@@ -82,6 +89,7 @@ resource "aws_security_group" "hkpc-jls-wrfeed" {
   }
 
   egress {
+    description = "Dyanmic high ports outbound to 152.1.1.231"
     from_port   = 1024
     to_port     = 65535
     protocol    = "tcp"
@@ -89,22 +97,24 @@ resource "aws_security_group" "hkpc-jls-wrfeed" {
   }
 
   egress {
+    description = "Dyanmic high ports outbound to 10.32.176.211"
     from_port   = 1024
     to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["10.32.176.211/32"]
   }
-  
+
   // FTP-Data
   egress {
-    description = "FTP-Data"
+    description = "FTP-Data outbound self access"
     from_port   = 1024
     to_port     = 65535
     protocol    = "tcp"
-    security_groups = ["sg-0f27d915082a302b7"]
-  } 
+    self        = true
+  }
 
   egress {
+    description = "MSSQL outbount access to internal"
     from_port   = 1433
     to_port     = 1433
     protocol    = "tcp"
@@ -112,13 +122,15 @@ resource "aws_security_group" "hkpc-jls-wrfeed" {
   }
 
   egress {
+    description = "FTP SFTP outbound to internet"
     from_port   = 21
-    to_port     = 22 
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
+    description = "HTTP outbount to internet"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -126,6 +138,7 @@ resource "aws_security_group" "hkpc-jls-wrfeed" {
   }
 
   egress {
+    description = "HTTPS outbount to internet"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
@@ -133,55 +146,63 @@ resource "aws_security_group" "hkpc-jls-wrfeed" {
   }
 
   egress {
+    description = "MySQL outbount to internal"
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/8"]
   }
-  
+
   egress {
+    description = "DNS outbount to internal"
     from_port   = 53
     to_port     = 53
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/8"]
   }
-  
+
   egress {
+    description = "DNS outbount to 162.0.0.0"
     from_port   = 53
     to_port     = 53
     protocol    = "tcp"
     cidr_blocks = ["162.0.0.0/8"]
   }
-  
+
   egress {
+    description = "DNS UDP outbound to internal"
     from_port   = 53
     to_port     = 53
     protocol    = "udp"
     cidr_blocks = ["10.0.0.0/8"]
   }
-  
+
   egress {
+    description = "DNS UDP outbount to 162.0.0.0"
     from_port   = 53
     to_port     = 53
     protocol    = "udp"
     cidr_blocks = ["162.0.0.0/8"]
   }
-  
+
   egress {
+    description = "NTP outbound to any"
     from_port   = 123
     to_port     = 123
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  } 
-  
+  }
+
   egress {
+    description = "NTP UDP outbound to any"
     from_port   = 123
     to_port     = 123
     protocol    = "udp"
     cidr_blocks = ["0.0.0.0/0"]
-  }  
-  
+  }
+
   egress {
+    description = "ICMP outbound to any"
     from_port   = -1
     to_port     = -1
     protocol    = "icmp"
@@ -189,20 +210,23 @@ resource "aws_security_group" "hkpc-jls-wrfeed" {
   }
 
   egress {
-    from_port   = 1433 
-    to_port     = 1433 
+    description = "MSSQL outbound to internal"
+    from_port   = 1433
+    to_port     = 1433
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/8"]
   }
 
   egress {
-    from_port   = 445 
-    to_port     = 445 
+    description = "SMB outbout to internal"
+    from_port   = 445
+    to_port     = 445
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/8"]
   }
 
   egress {
+    description = "SMTP outbout to any" // need to revisit this 
     from_port   = 25
     to_port     = 25
     protocol    = "tcp"
@@ -216,7 +240,7 @@ resource "aws_security_group" "hkpc-jls-wrfeed" {
 
 data "aws_ami" "hkpc-jls-wrfeed" {
   most_recent = true
-  owners   = ["819633815198"]  
+  owners      = ["819633815198"]
   filter {
     name   = "name"
     values = ["DJW2K19DC_VANILLA_PACKER_CHEF12*"]
@@ -224,28 +248,28 @@ data "aws_ami" "hkpc-jls-wrfeed" {
 }
 
 resource "aws_instance" "hkpc-jls-wrfeed" {
-    ami                    = "${data.aws_ami.hkpc-jls-wrfeed.image_id}"
-    instance_type          = "${var.instance_type}"
-    key_name               = "${aws_key_pair.hkpc-jls-wrfeed-key.id}" 
-    subnet_id              = "${var.subnet_id}" 
-    vpc_security_group_ids = ["${data.aws_security_group.djif-default-hkpc-jls-wrfeed.id}","${aws_security_group.hkpc-jls-wrfeed.id}"]
+  ami                    = data.aws_ami.hkpc-jls-wrfeed.image_id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.hkpc-jls-wrfeed-key.id
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = ["${data.aws_security_group.djif-default-hkpc-jls-wrfeed.id}", "${aws_security_group.hkpc-jls-wrfeed.id}"]
 
 
-    root_block_device {
-        volume_size = "${var.root_v_size}"
-        volume_type = "${var.root_v_type}"
-    }
+  root_block_device {
+    volume_size = var.root_v_size
+    volume_type = var.root_v_type
+  }
 
-    tags = {
-        Name        = "${var.hkpc-jls-wrfeed-name}" 
-        bu          = "djin"
-        owner       = "${var.TagOwner}"
-        environment = "${var.TagEnv}"
-        product     = "${var.TagProduct}"
-        component   = "${var.TagComponent}"
-        servicename = "djin/newswires/web"
-        appid       = "in_newswires_web_jlswireryter"   
-        autosnap    = "bkp=a"
-        preserve    = true
-    }
+  tags = {
+    Name        = "${var.hkpc-jls-wrfeed-name}"
+    bu          = "djin"
+    owner       = "${var.TagOwner}"
+    environment = "${var.TagEnv}"
+    product     = "${var.TagProduct}"
+    component   = "${var.TagComponent}"
+    servicename = "djin/newswires/web"
+    appid       = "in_newswires_web_jlswireryter"
+    autosnap    = "bkp=a"
+    preserve    = true
+  }
 }
