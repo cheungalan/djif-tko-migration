590bc5fa252afed3ed46d640fbc9ac78181d75a9
cheungalan <alan.cheung@dowjones.com>
2023-07-06 22:11:35 +0800

    Update hkpc-cls-recv-qa.tf
    Fix outbound SMTP to DJ SMTP Server only

--- accounts/connonprodwsjapac/tokyo/hkpc-cls-recv-qa.tf
+++ accounts/connonprodwsjapac/tokyo/hkpc-cls-recv-qa.tf
@@ -187,18 +187,20 @@ resource "aws_security_group" "hkpc-cls-recv" {
     cidr_blocks = ["10.0.0.0/8"]
   }
 
+/*
   egress {
     from_port   = 445
     to_port     = 445
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/8"]
   }
+*/
 
   egress {
     from_port   = 25
     to_port     = 25
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.13.32.134/32", "172.26.150.199/32"]
   }
 }
 
