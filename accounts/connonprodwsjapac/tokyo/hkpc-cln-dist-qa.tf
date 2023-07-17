3acf8de212fc6b0e2aa0fc3849d3971f93934352
cheungalan <alan.cheung@dowjones.com>
2023-07-17 13:01:22 +0800

    Update hkpc-cln-dist-qa.tf
    remove outbound FTP/SFTP (21-22)

--- accounts/connonprodwsjapac/tokyo/hkpc-cln-dist-qa.tf
+++ accounts/connonprodwsjapac/tokyo/hkpc-cln-dist-qa.tf
@@ -106,12 +106,14 @@ resource "aws_security_group" "hkpc-cln-dist" {
     cidr_blocks = ["10.0.0.0/8"]
   }
 
+/*
   egress {
     from_port   = 21
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+*/
 
   egress {
     description = "Internet Access 80"
