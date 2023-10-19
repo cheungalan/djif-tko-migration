0df1c940e2afef0894a6601db5a83b25c41c7d22
Ed Li <ed.li@dowjones.com>
2023-10-19 16:32:36 -0400

    AN-562 removed deprecated prod servers in prod

--- accounts/conprodwsjapac/tokyo/tko-rc-web.tf
+++ accounts/conprodwsjapac/tokyo/tko-rc-web.tf
@@ -1,15 +1,12 @@
-
-// Bring back tfstate in sync
-
 resource "aws_eip" "tko-rc-web-eip" {
-  count  = 3
-  domain = "vpc"
+  for_each = toset(["1", "3"])
+  domain   = "vpc"
 }
 
 resource "aws_eip_association" "tko-rc-web-eip-assoc" {
-  count               = 3
-  instance_id         = element(aws_instance.tko-rc-web.*.id, count.index)
-  allocation_id       = element(aws_eip.tko-rc-web-eip.*.id, count.index)
+  for_each            = toset(["1", "3"])
+  instance_id         = aws_instance.tko-rc-web[each.value].id
+  allocation_id       = aws_eip.tko-rc-web-eip[each.value].id
   allow_reassociation = false
 }
 
@@ -192,12 +189,12 @@ resource "aws_security_group_rule" "allow_rds_web_egress" {
 */
 
 resource "aws_instance" "tko-rc-web" {
-  count = 3
+  for_each = toset(["1", "3"])
   // keep the same config AMI  for the first two instances and let the new instances use the current AMI
-  ami                    = count.index < 2 ? data.aws_ami.tko_rc_web_image_old.image_id : data.aws_ami.tko_rc_web_image.image_id
+  ami                    = tonumber(each.value) < 3 ? data.aws_ami.tko_rc_web_image_old.image_id : data.aws_ami.tko_rc_web_image.image_id
   instance_type          = var.tko_rc_web_instance_type
   key_name               = aws_key_pair.tko_rc_web_key.id
-  subnet_id              = var.tko_rc_web_subnet_ids[count.index]
+  subnet_id              = var.tko_rc_web_subnet_ids[tonumber(each.value) - 1]
   vpc_security_group_ids = [data.aws_security_group.djif-default-web.id, aws_security_group.djif-rc-web-sg.id]
 
   root_block_device {
@@ -205,7 +202,7 @@ resource "aws_instance" "tko-rc-web" {
     volume_size           = var.root_v_size
     volume_type           = var.root_v_type
     tags = {
-      Name        = "${var.tko_rc_web_name}${count.index + 1}-root"
+      Name        = "${var.tko_rc_web_name}${each.value}-root"
       vollockname = "tko_rc_web_key-orig"
       bu          = var.TagBU
       owner       = var.TagOwner
@@ -218,7 +215,7 @@ resource "aws_instance" "tko-rc-web" {
   }
 
   tags = {
-    Name        = "${var.tko_rc_web_name}${count.index + 1}"
+    Name        = "${var.tko_rc_web_name}${each.value}"
     bu          = var.TagBU
     owner       = var.TagOwner
     environment = var.TagEnv
@@ -229,4 +226,52 @@ resource "aws_instance" "tko-rc-web" {
     preserve    = true
     autosnap    = "bkp=o"
   }
+}
+
+# moved aws_eip_association.tko-rc-web-eip-assoc to for_each
+moved {
+  from = aws_eip_association.tko-rc-web-eip-assoc[0]
+  to   = aws_eip_association.tko-rc-web-eip-assoc["1"]
+}
+
+moved {
+  from = aws_eip_association.tko-rc-web-eip-assoc[1]
+  to   = aws_eip_association.tko-rc-web-eip-assoc["2"]
+}
+
+moved {
+  from = aws_eip_association.tko-rc-web-eip-assoc[2]
+  to   = aws_eip_association.tko-rc-web-eip-assoc["3"]
+}
+
+// moved aws_instance.tko-rc-web to for_each
+moved {
+  from = aws_instance.tko-rc-web[0]
+  to   = aws_instance.tko-rc-web["1"]
+}
+
+moved {
+  from = aws_instance.tko-rc-web[1]
+  to   = aws_instance.tko-rc-web["2"]
+}
+
+moved {
+  from = aws_instance.tko-rc-web[2]
+  to   = aws_instance.tko-rc-web["3"]
+}
+
+# moved aws_eip.tko-rc-web-eip to index
+moved {
+  from = aws_eip.tko-rc-web-eip[0]
+  to   = aws_eip.tko-rc-web-eip["1"]
+}
+
+moved {
+  from = aws_eip.tko-rc-web-eip[1]
+  to   = aws_eip.tko-rc-web-eip["2"]
+}
+
+moved {
+  from = aws_eip.tko-rc-web-eip[2]
+  to   = aws_eip.tko-rc-web-eip["3"]
 }
