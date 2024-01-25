fd9fea7be2d3834ec06caa547349bf40f6f3454b
Ed Li <ed.li@dowjones.com>
2024-01-25 12:19:13 -0500

    CT-15762 use new keypair to the new QA servers.

--- accounts/connonprodwsjapac/tokyo/aws-dist-admin_amzn2023-qa.tf
+++ accounts/connonprodwsjapac/tokyo/aws-dist-admin_amzn2023-qa.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "dist_admin_amzn2023" {
 
   ami                    = data.aws_ami.amigo_amzn_linux2023.image_id
   instance_type          = var.instance_type
-  key_name               = aws_key_pair.hkpc-dist-admin-qa-key.id
+  key_name               = aws_key_pair.aws_wsjasia_key_qa.id
   subnet_id              = data.aws_subnets.protected[each.value].ids.0
   vpc_security_group_ids = [aws_security_group.aws-dist-admin-sg.id]
 
