764625af7485c78a4cb7f3433b571375f9913059
Ed Li <ed.li@dowjones.com>
2024-01-30 17:17:51 -0500

    fix module source path

--- accounts/conprodwsjapac/tokyo-common-sg/main.tf
+++ accounts/conprodwsjapac/tokyo-common-sg/main.tf
@@ -1,5 +1,5 @@
 module "sg" {
-  source       = "../../../../modules/security_groups"
+  source       = "../../../modules/security_groups"
   vpc_env      = var.vpc_env
   region_name  = local.region_name_by_id[var.aws_region]
   vpc_id       = data.aws_vpc.vpc.id
