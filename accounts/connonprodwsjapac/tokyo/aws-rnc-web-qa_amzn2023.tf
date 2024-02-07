d27054fe35ec3a6dd35c25a9f4b3c346e8a9e82d
cheungalan <alan.cheung@dowjones.com>
2024-02-07 23:39:22 +0800

    Update aws-rnc-web-qa_amzn2023.tf
    remove `aws-` from the SG's name

--- accounts/connonprodwsjapac/tokyo/aws-rnc-web-qa_amzn2023.tf
+++ accounts/connonprodwsjapac/tokyo/aws-rnc-web-qa_amzn2023.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "aws_rnc_web_amzn2023" {
   instance_type          = "t3.medium"
   key_name               = aws_key_pair.aws_wsjasia_key_qa.id
   subnet_id              = data.aws_subnets.protected[each.value].ids.0
-  vpc_security_group_ids = [data.aws_security_group.b_selected["wsjapac-default-sg"].id, aws_security_group.aws-rnc-web-sg.id]
+  vpc_security_group_ids = [data.aws_security_group.b_selected["wsjapac-default-sg"].id, aws_security_group.rnc-web-sg.id]
 
   metadata_options { // required IMDSV2
     http_endpoint          = "enabled"
