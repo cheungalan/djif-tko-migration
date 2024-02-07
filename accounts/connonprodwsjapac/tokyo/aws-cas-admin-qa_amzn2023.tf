b66671f26afa4160311317e2368ae7639c6c342b
cheungalan <alan.cheung@dowjones.com>
2024-02-07 23:34:47 +0800

    Update aws-cas-admin-qa_amzn2023.tf
    remove `aws-` from the SG's name

--- accounts/connonprodwsjapac/tokyo/aws-cas-admin-qa_amzn2023.tf
+++ accounts/connonprodwsjapac/tokyo/aws-cas-admin-qa_amzn2023.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "cas_admin_amzn2023" {
   instance_type          = var.instance_type
   key_name               = aws_key_pair.aws_wsjasia_key_qa.id
   subnet_id              = data.aws_subnets.protected[each.value].ids.0
-  vpc_security_group_ids = [data.aws_security_group.b_selected["wsjapac-default-sg"].id, aws_security_group.aws-cas-admin-sg.id]
+  vpc_security_group_ids = [data.aws_security_group.b_selected["wsjapac-default-sg"].id, aws_security_group.cas-admin-sg.id]
 
   metadata_options { // required IMDSV2
     http_endpoint          = "enabled"
