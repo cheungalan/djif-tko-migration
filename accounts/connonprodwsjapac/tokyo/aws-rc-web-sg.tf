860316bcfe9671020cdc02c1c76be92200d6ee2f
cheungalan <alan.cheung@dowjones.com>
2024-02-07 16:09:43 +0800

    Update aws-rc-web-sg.tf
    update security group to remove SG that's already setup in wsjapac_default

--- accounts/connonprodwsjapac/tokyo/aws-rc-web-sg.tf
+++ accounts/connonprodwsjapac/tokyo/aws-rc-web-sg.tf
@@ -12,14 +12,6 @@ resource "aws_security_group" "aws-rc-web-sg" {
   )
 
   ingress {
-    description = "SSH Access from Global Protect Subnet"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.197.240.0/20", "10.169.144.0/20", "10.140.16.0/20", "10.32.120.0/24", "10.193.240.0/20", "10.199.240.0/20"]
-  }
-
-  ingress {
     description     = "SSH/SFTP Access from hkpc-rc-datagen01_qa"
     from_port       = 22
     to_port         = 22
@@ -43,30 +35,6 @@ resource "aws_security_group" "aws-rc-web-sg" {
     cidr_blocks = ["10.0.0.0/8"]
   }
 
-  ingress {
-    description = "ICMP"
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["10.0.0.0/8"]
-  }
-
-  egress {
-    description = "Internet Access 80"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = "Internet Access 443"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     description     = "Access to RDS djcs-wsja-rds-qa.cluster-ckwswi0iistd.ap-northeast-1.rds.amazonaws.com"
     from_port       = 3306
@@ -75,43 +43,4 @@ resource "aws_security_group" "aws-rc-web-sg" {
     security_groups = ["sg-01536f4a5ec7e6519"]
   }
 
-  egress {
-    description = "DNS TCP"
-    from_port   = 53
-    to_port     = 53
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]
-  }
-
-  egress {
-    description = "DNS UDP"
-    from_port   = 53
-    to_port     = 53
-    protocol    = "udp"
-    cidr_blocks = ["10.0.0.0/8"]
-  }
-
-  egress {
-    description = "NTP TCP"
-    from_port   = 123
-    to_port     = 123
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]
-  }
-
-  egress {
-    description = "NTP UDP"
-    from_port   = 123
-    to_port     = 123
-    protocol    = "udp"
-    cidr_blocks = ["10.0.0.0/8"]
-  }
-
-  egress {
-    description = "ICMP"
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
