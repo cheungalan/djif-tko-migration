3e873dae5529fd7fbccd19bb01b772b7174048d8
cheungalan <alan.cheung@dowjones.com>
2024-02-08 11:28:04 +0800

    Update and rename aws-jls-wrweb_amzn2023.tf to aws-jwr-web_amzn2023.tf
    change instance name

--- accounts/conprodwsjapac/tokyo/aws-jwr-web_amzn2023.tf
+++ accounts/conprodwsjapac/tokyo/aws-jwr-web_amzn2023.tf
@@ -1,14 +1,18 @@
-resource "aws_instance" "jls_wrweb_amzn2023" {
+resource "aws_instance" "jwr_web_amzn2023" {
   for_each = {
-    AWS-JLS-WRWEB-11 = "a" // serverName = "AZ_short_id"
-    AWS-JLS-WRWEB-12 = "c"
+    tokpkjwrweb01 = "a" // serverName = "AZ_short_id"
+    tokpkjwrweb02 = "c"
   }
 
   ami                    = data.aws_ami.amigo_amzn_linux2023.image_id
   instance_type          = "t3.xlarge"
   key_name               = aws_key_pair.aws_wsjasia_key.id
   subnet_id              = data.aws_subnets.protected[each.value].ids.0
-  vpc_security_group_ids = [data.aws_security_group.b_selected["AWS-JLS-WRWEB-sg"].id]
+  vpc_security_group_ids = [
+    data.aws_security_group.b_selected["djif_default"].id,
+    data.aws_security_group.b_selected["wsjapac-default-sg"].id,
+    data.aws_security_group.b_selected["AWS-JLS-WRWEB-sg"].id
+  ]
 
   metadata_options { // required IMDSV2
     http_endpoint          = "enabled"
@@ -22,7 +26,7 @@ resource "aws_instance" "jls_wrweb_amzn2023" {
     encrypted   = true
 
     tags = merge(
-      local.default_tags_jls_wrweb,
+      local.default_tags_jwr_web,
       {
         Name   = "${each.key}-root"
         ticket = "CT-15763"
@@ -31,7 +35,7 @@ resource "aws_instance" "jls_wrweb_amzn2023" {
   }
 
   tags = merge(
-    local.default_tags_jls_wrweb,
+    local.default_tags_jwr_web,
     {
       Name     = each.key
       autosnap = "bkp=o"
