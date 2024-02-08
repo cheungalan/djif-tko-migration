5e2105ef53589517572652ca08b1578936397330
cheungalan <alan.cheung@dowjones.com>
2024-02-08 14:05:34 +0800

    Update and rename aws-rc-web_amzn2023.tf to aws-rnc-web_amzn2023.tf
    change instance name

--- accounts/conprodwsjapac/tokyo/aws-rnc-web_amzn2023.tf
+++ accounts/conprodwsjapac/tokyo/aws-rnc-web_amzn2023.tf
@@ -1,14 +1,18 @@
-resource "aws_instance" "aws_rc_web_amzn2023" {
+resource "aws_instance" "aws_rnc_web_amzn2023" {
   for_each = {
-    AWS-RC-WEB-11 = "a" // serverName = "AZ_short_id"
-    AWS-RC-WEB-12 = "c"
+    tokpkrncweb01 = "a" // serverName = "AZ_short_id"
+    tokpkrncweb02 = "c"
   }
 
   ami                    = data.aws_ami.amigo_amzn_linux2023.image_id
   instance_type          = "t3.large"
   key_name               = aws_key_pair.aws_wsjasia_key.id
   subnet_id              = data.aws_subnets.protected[each.value].ids.0
-  vpc_security_group_ids = [data.aws_security_group.djif-default-web.id, aws_security_group.djif-rc-web-sg.id]
+  vpc_security_group_ids = [
+    data.aws_security_group.b_selected["djif_default"].id, 
+    data.aws_security_group.b_selected["wsjapac-default-sg"].id,
+    aws_security_group.djif-rc-web-sg.id
+  ]
 
   metadata_options { // required IMDSV2
     http_endpoint          = "enabled"
@@ -23,7 +27,7 @@ resource "aws_instance" "aws_rc_web_amzn2023" {
     delete_on_termination = false
 
     tags = merge(
-      local.default_tags_rc_web,
+      local.default_tags_rnc_web,
       {
         Name   = "${each.key}-root"
         ticket = "CT-15763"
@@ -32,7 +36,7 @@ resource "aws_instance" "aws_rc_web_amzn2023" {
   }
 
   tags = merge(
-    local.default_tags_rc_web,
+    local.default_tags_rnc_web,
     {
       Name     = each.key
       autosnap = "bkp=o"
