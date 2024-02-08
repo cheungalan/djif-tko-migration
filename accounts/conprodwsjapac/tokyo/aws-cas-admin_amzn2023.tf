df7c1217112345cf4e4a6b62f3d633d1eb0a9167
cheungalan <alan.cheung@dowjones.com>
2024-02-08 11:26:48 +0800

    Update and rename aws-dist-admin_amzn2023.tf to aws-cas-admin_amzn2023.tf
    change instance name

--- accounts/conprodwsjapac/tokyo/aws-cas-admin_amzn2023.tf
+++ accounts/conprodwsjapac/tokyo/aws-cas-admin_amzn2023.tf
@@ -1,13 +1,18 @@
-resource "aws_instance" "dist_admin_amzn2023" {
+resource "aws_instance" "cas_admin_amzn2023" {
   for_each = {
-    AWS-DIST-ADMIN-11 = "a" // serverName = "AZ_short_id"
+    tokpkcasadmin01 = "a" // serverName = "AZ_short_id"
   }
 
   ami                    = data.aws_ami.amigo_amzn_linux2023.image_id
   instance_type          = "t3.large"
   key_name               = aws_key_pair.aws_wsjasia_key.id
   subnet_id              = data.aws_subnets.protected[each.value].ids.0
-  vpc_security_group_ids = [data.aws_security_group.b_selected["AWS-DIST-ADMIN-sg"].id]
+  vpc_security_group_ids = [
+    data.aws_security_group.b_selected["djif_default"].id,
+    data.aws_security_group.b_selected["wsjapac-default-sg"].id,
+    data.aws_security_group.b_selected["AWS-DIST-ADMIN-sg"].id
+  ]
+
   metadata_options { // required IMDSV2
     http_endpoint          = "enabled"
     http_tokens            = "required"
@@ -20,7 +25,7 @@ resource "aws_instance" "dist_admin_amzn2023" {
     encrypted   = true
 
     tags = merge(
-      local.default_tags_dist_admin,
+      local.default_tags_cas_admin,
       {
         Name   = "${each.key}-root"
         ticket = "CT-15763"
@@ -29,7 +34,7 @@ resource "aws_instance" "dist_admin_amzn2023" {
   }
 
   tags = merge(
-    local.default_tags_dist_admin,
+    local.default_tags_cas_admin,
     {
       Name     = each.key
       autosnap = "bkp=o"
