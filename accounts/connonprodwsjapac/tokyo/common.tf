f55a27bc0600ee4a62d83e9d8f07e716cc26aafe
Ed Li <ed.li@dowjones.com>
2024-04-09 10:21:17 -0400

    Move  default amzn linux 2023 AMI to account level

--- accounts/connonprodwsjapac/tokyo/common.tf
+++ accounts/connonprodwsjapac/tokyo/common.tf
@@ -1,1 +1,8 @@
 ## common by environment 
+data "aws_ami" "amigo_amzn_linux2023" {
+  owners = ["528339170479"]
+  filter {
+    name   = "name"
+    values = ["amigo-amzn_linux-2023-dowjones-base-202401042335"]
+  }
+}
