726687554ea9246e5cb4b837e65cda9d26e89e2c
cheungalan <alan.cheung@dowjones.com>
2024-04-09 15:21:01 +0800

    Update aws-jwr-web_sg.tf

--- accounts/conprodwsjapac/tokyo/aws-jwr-web_sg.tf
+++ accounts/conprodwsjapac/tokyo/aws-jwr-web_sg.tf
@@ -12,19 +12,19 @@ resource "aws_security_group" "jwr-web-sg" {
   )
 
   ingress {
-    description = "HTTP access from internet"
+    description = "HTTP access from internal"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/8"]
   }
 
   ingress {
-    description = "HTTPS access from internet"
+    description = "HTTPS access from internal"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/8"]
   }
 
   // 3306 Access AWS-JLS-Feed-sg
