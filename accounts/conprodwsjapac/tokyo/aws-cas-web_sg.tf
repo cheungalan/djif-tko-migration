afc1b94a811beb88d95d0acb7e8792f4ac6c460a
cheungalan <alan.cheung@dowjones.com>
2024-04-09 14:34:44 +0800

    Update aws-cas-web_sg.tf

--- accounts/conprodwsjapac/tokyo/aws-cas-web_sg.tf
+++ accounts/conprodwsjapac/tokyo/aws-cas-web_sg.tf
@@ -1,6 +1,6 @@
-resource "aws_security_group" "cas_web-sg" {
-  name        = "cas_web-sg"
-  description = "cas_web-sg"
+resource "aws_security_group" "cas-web-sg" {
+  name        = "cas-web-sg"
+  description = "cas-web-sg"
   vpc_id      = var.vpc_id
 
   tags = merge(
@@ -8,7 +8,7 @@ resource "aws_security_group" "cas_web-sg" {
     {
       Name     = "cas-web-sg"
       preserve = true
-      ticket   = "CT-15762"
+      ticket   = "CT-15847"
     }
   )
 
