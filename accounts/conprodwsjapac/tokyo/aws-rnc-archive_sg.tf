03e4e6170ec3cf99ed1df613771148ceee60a190
cheungalan <alan.cheung@dowjones.com>
2024-04-19 11:31:54 +0800

    Update aws-rnc-archive_sg.tf
    - Add SSH outbound access to tokpkrncweb
    - Remove inbound 80/443 access

--- accounts/conprodwsjapac/tokyo/aws-rnc-archive_sg.tf
+++ accounts/conprodwsjapac/tokyo/aws-rnc-archive_sg.tf
@@ -11,22 +11,6 @@ resource "aws_security_group" "rnc-archive-sg" {
     }
   )
 
-  ingress {
-    description = "HTTP access from internet"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTPS access from internet"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   // Allow rDS access 
   egress {
     description     = "Access to RDS djcs-wsja-rds-prod.cluster-c1qsnfwzpreu.ap-northeast-1.rds.amazonaws.com"
@@ -43,6 +27,15 @@ resource "aws_security_group" "rnc-archive-sg" {
     to_port         = 22
     protocol        = "tcp"
     security_groups = ["sg-06e24f4c64f2dad71"]
+  }
+
+  // SSH access to tko-rc-web 
+  egress {
+    description     = "SSH outbound access to tokpkrncweb"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.rnc-web-sg.id]
   }
 
 }
